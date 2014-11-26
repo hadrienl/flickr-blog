c655410ae6e77b326bbc24634622f3046054a980
Hadrien Lanneau <hadrien@lanneau.me>
2014-11-26 23:21:01 +0100

    Clean sync

--- core/sync.js
+++ core/sync.js
@@ -9,47 +9,13 @@ function processCollection (client, collection) {
     .synchronize(collection)
     .then(function () {
       return q.all(collection.set.map(function (set) {
-        // Create photosets
-        return q.ninvoke(
-            client,
-            'executeAPIRequest',
-            'flickr.photosets.getInfo',
-            {
-              'photoset_id': set.id
-            },
-            true
-          )
-        .then(function (data) {
-          return database.PhotoSet.synchronize({
-            id: data.photoset.id,
-            title: data.photoset.title._content,
-            description: data.photoset.description._content,
-            date_create: data.photoset.date_create,
-            date_update: data.photoset.date_update
-          });
-        });
+        return processSet(client, set);
       }));
     })
     .then(function (data) {
       // Read photos for each photoset
       return q.all(data.map(function (set) {
-        return q.ninvoke(
-            client,
-            'executeAPIRequest',
-            'flickr.photosets.getPhotos',
-            {
-              'photoset_id': set.orig_id,
-              'extras': 'tags, media, url_sq, url_t, url_s, url_m, url_o',
-              'per_page': 9999
-            },
-            true
-          )
-          // Write each photo in database
-          .then(function (data) {
-            return q.all(data.photoset.photo.map(function (photo) {
-              return database.Photo.synchronize(photo);
-            }));
-          });
+        return processPhotos(client, set);
       }));
     })
     .then(function (photos) {
@@ -58,6 +24,67 @@ function processCollection (client, collection) {
     .catch(function (err) {
       console.error(err);
     });
+
+  return deferred.promise;
+}
+
+function processSet(client, set) {
+  var deferred = q.defer();
+
+  q.ninvoke(
+      client,
+      'executeAPIRequest',
+      'flickr.photosets.getInfo',
+      {
+        'photoset_id': set.id
+      },
+      true
+    )
+    .then(function (data) {
+      return database.PhotoSet.synchronize({
+        id: data.photoset.id,
+        title: data.photoset.title._content,
+        description: data.photoset.description._content,
+        date_create: data.photoset.date_create,
+        date_update: data.photoset.date_update
+      });
+    })
+    .then(function (data) {
+      deferred.resolve(data);
+    })
+    .catch(function (err) {
+      deferred.reject(err);
+    });
+
+  return deferred.promise;
+}
+
+function processPhotos (client, set) {
+  var deferred = q.defer();
+
+  q.ninvoke(
+    client,
+    'executeAPIRequest',
+    'flickr.photosets.getPhotos',
+    {
+      'photoset_id': set.orig_id,
+      'extras': 'tags, media, url_sq, url_t, url_s, url_m, url_o',
+      'per_page': 9999
+    },
+    true
+  )
+  // Write each photo in database
+  .then(function (data) {
+    return q.all(data.photoset.photo.map(function (photo) {
+      return database.Photo.synchronize(photo);
+    }));
+  })
+  .then(function () {
+    deferred.resolve();
+  })
+  .catch(function (err) {
+    deferred.reject(err);
+  });
 
   return deferred.promise;
 }
@@ -78,6 +105,9 @@ module.exports = function (client) {
       return q.all(res.collections.collection.map(function (collection) {
         return processCollection(client, collection);
       }));
+    })
+    .then(function () {
+      deferred.resolve();
     })
     .catch (function (error) {
       console.error(error);
