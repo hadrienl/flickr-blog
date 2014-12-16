daf03dcd83cdcf5d97b91fd7e4a37ddc9609d479
Hadrien Lanneau <hadrien@lanneau.me>
2014-12-16 21:42:41 +0100

    Display a nice feed thanks to theme data mutation

--- themes/default/index.js
+++ themes/default/index.js
@@ -3,6 +3,31 @@ module.exports = {
     return data;
   },
   photoset: function (data) {
+    // Make two smart columns of photos
+    sortPhotos(data.photos);
     return data;
   }
 };
+
+function sortPhotos(photos) {
+  var breakRow = 0;
+
+  photos.forEach(function (photo, index) {
+    if (breakRow === 0) {
+      photo.startRow = true;
+    }
+    if (photo.landscape ||
+        breakRow !== 1 && (!photos[index + 1] || photos[index + 1].landscape)) {
+      photo.colWidth = 2;
+      photo.endRow = true;
+      breakRow = 0;
+    } else {
+      photo.colWidth = 1;
+      breakRow++;
+      if (breakRow > 1) {
+        photo.endRow = true;
+        breakRow = 0;
+      }
+    }
+  });
+}
